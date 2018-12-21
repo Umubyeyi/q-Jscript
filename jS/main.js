54bdfa957ecf3396b6be98169f33c85a3551aaf8
umubyeyi dinah <dinahu2020@gmail.com>
2018-12-21 14:36:53 +0200

    I add the alert of score my quiz-board

---
+++ jS/main.js
@@ -0,0 +1,46 @@
+
+            var userInput = new Array();
+            var answers = new Array(10);
+            answers[0] = "B";
+            answers[1] = "C";
+            answers[2] = "A";
+            answers[3] = "C";
+            
+            
+            function getScore(){
+            var score=0;
+            var numQuestions=4;
+            
+            for (var i=0;i<numQuestions;i++){
+            if (userInput[i]==answers[i]){
+            score += 1;
+            }
+            else{
+            score += 0;
+            }
+            
+            }
+            return score;
+            }
+            function returnScore(){
+            alert("Your score is "+getScore()+"/10");
+            }
+            var answers = ["A","C","B"], 
+            tot = answers.length;
+            
+            function getCheckedValue( radioName ){
+            var radios = document.getElementsByName( radioName ); // Get radio group by-name
+            for(var y=0; y<radios.length; y++)
+              if(radios[y].checked) return radios[y].value; // return the checked value
+            }
+            
+            function getScore(){
+            var score = 0;
+            for (var i=0; i<tot; i++)
+            if(getCheckedValue("question"+i)===answers[i]) score += 1; // increment only
+            return score;
+            }
+            
+            function returnScore(){
+            alert("Your score is "+ getScore() +"/"+ tot);
+            }
